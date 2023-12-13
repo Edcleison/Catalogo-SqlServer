ef065522da595c3b07bb3a527de4769c0db5bfd7
Edcleison <edcleisonbrito@gmail.com>
2023-12-13 14:01:07 -0300

    first commit

---
+++ Catalogo.Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Catalogo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Infra.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    /*
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = CatalogoDB");
+    }
+    public ApplicationDbContext()
+    {
+    }
+    */
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext)
+            .Assembly);
+    }
+}
